1157d5242a1eff4fbb2303714f3069b77334f162
Mykola <Olli25010@gmail.com>
2019-11-03 11:02:29 +0200

    finished small game on rust

--- task1/src/main.rs
+++ task1/src/main.rs
@@ -5,18 +5,24 @@ use std::cmp::Ordering;
 fn main() {
     println!("Guess a number");
     let secret_number = rand::thread_rng().gen_range(1, 101);
-    println!("Pleas, input your guess");
-    println!("the sicret number is {}", secret_number);
-    let mut guess = String::new();
-    io::stdin().read_line(&mut guess)
-        .expect("Failed line");
-    let guess: u32 = guess.trim().parse()
-        .expect("Please type a number!");
-    println!("You guessed:{}", guess);
-    match guess.cmp(&secret_number) {
-        Ordering::Less => println!("Too small!"),
-        Ordering::Greater => println!("Too big"),
-        Ordering::Equal => println!("You winn!")
+//    println!("the sicret number is {}", secret_number);
+    loop {
+        println!("Pleas, input your guess");
+        let mut guess = String::new();
+        io::stdin().read_line(&mut guess)
+            .expect("Failed line");
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+        println!("You guessed:{}", guess);
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
+        }
     }
-
 }
