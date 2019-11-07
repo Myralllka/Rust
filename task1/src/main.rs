f56d5cfec9c7267125bb76e80857f440b011df53
Mykola <Olli25010@gmail.com>
2019-11-07 17:50:17 +0200

    functions

--- task1/src/main.rs
+++ task1/src/main.rs
@@ -1,28 +1,31 @@
-use std::io;
-use rand::Rng;
-use std::cmp::Ordering;
+//fn main() {
+//    const MAX:u32 = 234;
+//    let mut x = 5;
+//    println!("val {}", x);
+//    x = 6;
+//    println!("val {}", x);
+//}
+
+#![allow(unused_variables)]
 
 fn main() {
-    println!("Guess a number");
-    let secret_number = rand::thread_rng().gen_range(1, 101);
-//    println!("the sicret number is {}", secret_number);
-    loop {
-        println!("Pleas, input your guess");
-        let mut guess = String::new();
-        io::stdin().read_line(&mut guess)
-            .expect("Failed line");
-        let guess: u32 = match guess.trim().parse() {
-            Ok(num) => num,
-            Err(_) => continue,
-        };
-        println!("You guessed:{}", guess);
-        match guess.cmp(&secret_number) {
-            Ordering::Less => println!("Too small!"),
-            Ordering::Greater => println!("Too big"),
-            Ordering::Equal => {
-                println!("You win!");
-                break;
-            }
-        }
-    }
+    let x = 1;
+    let x = x + 1;
+    let x = x + 2;
+//    println!("{}", x)
+    let spaces = "    ";
+    let spaces = spaces.len();
+    let guess: u32 = "42".parse().expect("Error");
+    let float = 2.0;
+    let y: f32 = 4.0;
+    let t = true;
+    let f: bool = false;
+    let heart_eyed_cat = '😻';
+    let tup = (1, 2, 3, 4, 5);
+    let (x, y, z, c, v) = tup;
+    let one = tup.0;
+    let array = [1, 2, 3, 4, 5];
+    let months = ["January", "February", "March", "April", "May", "June", "July",
+        "August", "September", "October", "November", "December"];
+
 }
