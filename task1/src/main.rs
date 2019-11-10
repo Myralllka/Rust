b74252bd48f057f2a51d8187cf6949f3cfb372d7
Mykola <Olli25010@gmail.com>
2019-11-10 17:49:38 +0200

    finished functions

--- task1/src/main.rs
+++ task1/src/main.rs
@@ -1,31 +1,52 @@
-//fn main() {
-//    const MAX:u32 = 234;
-//    let mut x = 5;
-//    println!("val {}", x);
-//    x = 6;
-//    println!("val {}", x);
-//}
-
-#![allow(unused_variables)]
-
 fn main() {
-    let x = 1;
-    let x = x + 1;
-    let x = x + 2;
-//    println!("{}", x)
-    let spaces = "    ";
-    let spaces = spaces.len();
-    let guess: u32 = "42".parse().expect("Error");
-    let float = 2.0;
-    let y: f32 = 4.0;
-    let t = true;
-    let f: bool = false;
-    let heart_eyed_cat = '😻';
-    let tup = (1, 2, 3, 4, 5);
-    let (x, y, z, c, v) = tup;
-    let one = tup.0;
-    let array = [1, 2, 3, 4, 5];
-    let months = ["January", "February", "March", "April", "May", "June", "July",
-        "August", "September", "October", "November", "December"];
 
+
+    let mut number:i32 = 3;
+    while number != 0 {
+        println!("{}!", number);
+        number -= 1;
+    }
+    println!("LIFTOFF!!!");
+    let a = [10, 20, 30, 40, 50];
+    for element in a.iter() {
+        println!("the value is: {}", element);
+    }
+
+    println!("Hello world");
+    let x = 65;
+    let z = {
+        let x = 6;
+        x + 1
+    };
+    println!("{}", z);
+    func2(z);
+    let num = 8;
+    if num < 10 {
+        println!("less 10")
+    } else {
+        println!("more 10")
+    }
+    let number = {
+        if num < 10 {
+            69
+        } else {
+            96
+        }
+    };
+    println!("number {}", number);
+    let mut counter: i32 = 0;
+
+    let result: i32 = loop {
+        counter += 1;
+
+        if counter == 10 {
+            break counter * 2;
+        }
+    };
+
+    println!("The result is {}", result);
+}
+
+fn func2(number: i32) {
+    println!("Hello {}", number);
 }
---
+++ task1/src/vars.rs
@@ -0,0 +1,31 @@
+//fn main() {
+//    const MAX:u32 = 234;
+//    let mut x = 5;
+//    println!("val {}", x);
+//    x = 6;
+//    println!("val {}", x);
+//}
+
+#![allow(unused_variables)]
+
+fn main() {
+    let x = 2;
+    let x = x + 1;
+    let x = x + 2;
+//    println!("{}", x)
+    let spaces = "    ";
+    let spaces = spaces.len();
+    let guess: u32 = "42".parse().expect("Error");
+    let float = 2.0;
+    let y: f32 = 4.0;
+    let t = true;
+    let f: bool = false;
+    let heart_eyed_cat = '😻';
+    let tup = (1, 2, 3, 4, 5);
+    let (x, y, z, c, v) = tup;
+    let one = tup.0;
+    let array = [1, 2, 3, 4, 5];
+    let months = ["January", "February", "March", "April", "May", "June", "July",
+        "August", "September", "October", "November", "December"];
+
+}
